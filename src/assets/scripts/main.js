24d2f6d11688b538cced40690fe831e82a07b03d
Moisés Segura Cedrés <msegurace@uoc.edu>
2023-04-07 10:24:13 +0100

    deploy to netlify

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,15 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+
+const { readFileSync } = require('fs')
+
+const posthtml = require('posthtml')
+const include = require('posthtml-include')
+
+const html = readFileSync('index.html')
+
+posthtml([ include({ encoding: 'utf8' }) ])
+    .process(html)
+    .then((result) => console.log(result.html))
